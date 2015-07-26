3828d675e44cde379ae4e90f4352c705859a522a
Jen Scobie <jenscobie@gmail.com>
2015-07-26 11:46:46 -0700

    Install virtualbox

--- spec/default/sample_spec.rb
+++ spec/default/sample_spec.rb
@@ -2,9 +2,18 @@ require 'spec_helper'
 
 describe command('java -version') do
   its(:exit_status) { should eq 0 }
-  its(:stderr) { should match /1.8/ }
+  its(:stderr) { should match /1.7/ }
 end
 
 describe package('git') do
   it { should be_installed }
+end
+
+describe package('unzip') do
+  it { should be_installed }
+end
+
+describe command('VBoxManage -v') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /5.0.0/ }
 end
